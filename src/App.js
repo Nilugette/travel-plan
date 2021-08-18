11683c02ae2d6daacaf8ee46e488fb34c2254fc3
Laure <nilugette@yahoo.fr>
2021-08-18 16:09:31 +0200

    Restaurant selection by clicking on the Map and scroll it on the sidebar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ const App = () => {
   const [places, setPlaces] = useState([])
   const [coordinates, setCoordinates] = useState({})
   const [bounds, setBounds] = useState({})
+  const [childClicked, setChildClicked] = useState(null)
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(()=> {
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude }}) => {
@@ -18,9 +20,11 @@ const App = () => {
   }, []) // the empty array means that this function will happen only at the start of the application
 
   useEffect(()=> {
+    setIsLoading(true);
     getPlacesData(bounds.sw, bounds.ne).then((data) => {
       console.log(data)
       setPlaces(data)
+      setIsLoading(false);
     })
   }, [coordinates, bounds]) 
   
@@ -30,14 +34,15 @@ const App = () => {
         <Header />
         <Grid container spacing={3} style={{ width : '100%'}}>
           <Grid item xs={12} md={4}>
-            <List places={places} />
+            <List places={places} childClicked={childClicked} isLoading={isLoading} />
           </Grid>
           <Grid item xs={12} md={8}>
             <Map 
               setCoordinates={setCoordinates}
               setBounds={setBounds}
               coordinates={coordinates}
-              places={places}  />
+              places={places}
+              setChildClicked={setChildClicked}  />
           </Grid>
         </Grid>
     </>
