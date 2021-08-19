d0fa15ffafccb614e5d8ad709a4102e278a0a3b5
Laure <nilugette@yahoo.fr>
2021-08-19 11:00:21 +0200

    Display the different type depending on select value and ratings

--- src/App.js
+++ src/App.js
@@ -7,26 +7,37 @@ import List from './components/List/List';
 import Map from './components/Map/Map'
 
 const App = () => {
+  
   const [places, setPlaces] = useState([])
-  const [coordinates, setCoordinates] = useState({})
+  const [coords, setCoords] = useState({});
   const [bounds, setBounds] = useState({})
   const [childClicked, setChildClicked] = useState(null)
   const [isLoading, setIsLoading] = useState(false);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
 
-  useEffect(()=> {
-    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude }}) => {
-      setCoordinates({ lat: latitude, lng: longitude})
-    })
-  }, []) // the empty array means that this function will happen only at the start of the application
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoords({ lat: latitude, lng: longitude });
+    });
+  }, []); // the empty array means that this function will happen only at the start of the application
+
+  useEffect(() => {
+    const filtered = places.filter((place) => Number(place.rating) >= rating);
+
+    setFilteredPlaces(filtered);
+  }, [rating]); // apply when rating is updating
 
   useEffect(()=> {
     setIsLoading(true);
-    getPlacesData(bounds.sw, bounds.ne).then((data) => {
-      console.log(data)
-      setPlaces(data)
+    getPlacesData(type, bounds.sw, bounds.ne)
+    .then((data) => {
+      setPlaces(data);
+      setFilteredPlaces([]);
       setIsLoading(false);
-    })
-  }, [coordinates, bounds]) 
+    });
+  }, [type, coords, bounds]) // apply when type or coords or bounds are updating
   
   return (
     <>
@@ -34,15 +45,24 @@ const App = () => {
         <Header />
         <Grid container spacing={3} style={{ width : '100%'}}>
           <Grid item xs={12} md={4}>
-            <List places={places} childClicked={childClicked} isLoading={isLoading} />
+            <List 
+              places={filteredPlaces.length ? filteredPlaces : places} 
+              childClicked={childClicked} 
+              isLoading={isLoading}
+              type={type}
+              setType={setType}
+              rating={rating}
+              setRating={setRating} 
+             />
           </Grid>
           <Grid item xs={12} md={8}>
             <Map 
-              setCoordinates={setCoordinates}
+              setCoordinates={setCoords}
               setBounds={setBounds}
-              coordinates={coordinates}
-              places={places}
-              setChildClicked={setChildClicked}  />
+              coordinates={coords}
+              places={filteredPlaces.length ? filteredPlaces : places}
+              setChildClicked={setChildClicked}
+              />
           </Grid>
         </Grid>
     </>
--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,20 @@
-import axios from 'axios'
+import axios from 'axios' 
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
     try {
-        const { data: {data} } = await axios.get(URL, {
+          const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
             params: {
               bl_latitude: sw.lat,
-              tr_latitude: ne.lat,
               bl_longitude: sw.lng,
-              tr_longitude: ne.lng
+              tr_longitude: ne.lng,
+              tr_latitude: ne.lat,
             },
             headers: {
               'x-rapidapi-key': 'dc901dbc7emsh8239ec6164aa79fp1be912jsn8f7ef4054f5a',
-              'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
-            }
-          })
+              'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            },
+          });
         return data
     } catch (error) {
         console.log(error)
